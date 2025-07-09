9278aeb409f128074424d4524e4373e587dd67a6
HyperLexus <you@example.com>
2025-07-10 01:01:22 +0200

    fix

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rr-fc-gen
 generates all possible fcs with all pids
+
+## how to use:
+- set total pids to use to desired number (1 billion generates all possible fcs)
+- run main.py
+
+ggwp
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from generate_fcs import generate_fcs
 
 if __name__ == "__main__":
-    generate_fcs(4_000_000)
+    total_pids_to_use = 1_000_000_000  # max is 1 billion. takes 3 hours!!
+    generate_fcs(total_pids_to_use)
